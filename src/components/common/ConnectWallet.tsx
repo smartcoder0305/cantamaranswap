4ea029a7072e9c2312936a5a8cef881eb0d08af8
rk1129 <ryan.kennedy1129@gmail.com>
2023-03-15 14:18:52 +0000

    Remove some part of wallet connect

--- src/components/common/ConnectWallet.tsx
+++ src/components/common/ConnectWallet.tsx
@@ -19,23 +19,7 @@ function authenticate() {
   });
 }
 
-function disconnect() {
-  userSession.signUserOut("/");
-}
-
 const ConnectWallet = () => {
-  if (userSession.isUserSignedIn()) {
-    return (
-      <div>
-        <button className="Connect" onClick={disconnect}>
-          Disconnect Wallet
-        </button>
-        <p>mainnet: {userSession.loadUserData().profile.stxAddress.mainnet}</p>
-        <p>testnet: {userSession.loadUserData().profile.stxAddress.testnet}</p>
-      </div>
-    );
-  }
-
   return (
     <button
       className="bg-[rgba(255,255,255,0.1)] px-6 py-3 rounded-full text-base font-light leading-6 border-special-black border-[1px] dark:border-none"
