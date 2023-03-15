4eba771fac482e6c8d6b8761ac17a6ed0bd03b84
rk1129 <ryan.kennedy1129@gmail.com>
2023-03-15 16:34:30 +0000

    Add swap view

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -79,6 +79,58 @@ const Home = () => {
               </p>
             </div>
           </div>
+          <div className="w-[520px] flex flex-col gap-5">
+            <div className="w-full flex rounded-[18px] bg-[rgba(11,11,15,0.9)] p-2 gap-2.5">
+              <p className="flex-1 flex items-center justify-center py-2 bg-[#14151A] rounded-2xl text-base font-normal">
+                Catamaran Swap
+              </p>
+              <p className="flex-1 flex items-center justify-center py-2 rounded-2xl text-base font-normal">
+                STX Swap
+              </p>
+              <p className="flex-1 flex items-center justify-center py-2 rounded-2xl text-base font-normal">
+                NFT Swap
+              </p>
+            </div>
+            <div className="w-full p-5 flex flex-col gap-3 bg-[rgba(11,11,15,0.9)] rounded-[18px]">
+              <div className="p-5 flex justify-between items-center rounded-lg bg-[#14151A]">
+                <div>
+                  <p className="text-xs font-light leading-[14px] opacity-50">
+                    You send
+                  </p>
+                  <p className="mt-2 text-[28px] leading-6 font-light">1</p>
+                  <p className="mt-4 text-xs leading-[14px] font-light opacity-50">
+                    ≈$275,208
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <img className="h-7 w-7" src={StxImg} alt="" />
+                  <p className="text-xl font-medium leading-6">STX</p>
+                </div>
+              </div>
+              <div className="p-5 flex justify-between items-center rounded-lg bg-[#14151A]">
+                <div>
+                  <p className="text-xs font-light leading-[14px] opacity-50">
+                    You receive
+                  </p>
+                  <p className="mt-2 text-[28px] leading-6 font-light">
+                    0.000035
+                  </p>
+                  <p className="mt-4 text-xs leading-[14px] font-light opacity-50">
+                    ≈$275,469
+                    <span className="ml-1 text-[#559276]">(0.0965%)</span>
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <img className="h-7 w-7" src={BtcImg} alt="" />
+                  <p className="text-xl font-medium leading-6">BTC</p>
+                </div>
+              </div>
+              <p className="px-10 py-2 text-sm leading-5 font-light">
+                1 BTC = 0.0004354 STX
+                <span className="opacity-50"> ($1.00043)</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
