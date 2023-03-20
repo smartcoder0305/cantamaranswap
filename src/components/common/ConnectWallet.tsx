21fb1a4e0ed2fe9eb90fa8454f7e5f26ff21d1a1
rk1129 <ryan.kennedy1129@gmail.com>
2023-03-20 16:22:26 +0000

    Complet wallet connect

--- src/components/common/ConnectWallet.tsx
+++ src/components/common/ConnectWallet.tsx
@@ -5,28 +5,35 @@ const appConfig = new AppConfig(["store_write", "publish_data"]);
 
 export const userSession = new UserSession({ appConfig });
 
-function authenticate() {
-  showConnect({
-    appDetails: {
-      name: "Stacks React Starter",
-      icon: window.location.origin + "/logo512.png",
-    },
-    redirectTo: "/",
-    onFinish: () => {
-      window.location.reload();
-    },
-    userSession,
-  });
-}
-
 const ConnectWallet = () => {
+  const address = userSession.loadUserData().profile.stxAddress
+    .mainnet as string;
+  const isAuthenticated = userSession.isUserSignedIn();
+
+  const authenticate = () => {
+    if (isAuthenticated === false)
+      showConnect({
+        appDetails: {
+          name: "Stacks React Starter",
+          icon: window.location.origin + "/logo512.png",
+        },
+        redirectTo: "/",
+        onFinish: () => {
+          window.location.reload();
+        },
+        userSession,
+      });
+  };
+
   return (
     <button
       className="bg-[rgba(255,255,255,0.1)] px-6 py-3 rounded-full text-base font-light leading-6 border-special-black border-[1px] dark:border-none"
       type="button"
       onClick={authenticate}
     >
-      Connect Wallet
+      {isAuthenticated
+        ? `${address.slice(0, 5)}...${address.slice(-3)}`
+        : "Connect Wallet"}
     </button>
   );
 };
