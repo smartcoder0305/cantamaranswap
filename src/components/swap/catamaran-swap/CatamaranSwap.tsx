51c9fde7e8314efe775b77a7b033667a8cffe235
smartcoder0305 <102741052+smartcoder0305@users.noreply.github.com>
2023-05-17 07:33:54 +0200

    # fix catamaranswap added tooltip

--- src/components/swap/catamaran-swap/CatamaranSwap.tsx
+++ src/components/swap/catamaran-swap/CatamaranSwap.tsx
@@ -133,7 +133,16 @@ const CatamaranSwap = ({
       <div className="w-full flex justify-between items-center">
         <div className="flex gap-2 items-center">
           <p className="text-base leading-6 font-normal">Catamaran Swap</p>
-          <InfoImg className="dark:stroke-white stroke-special-black" />
+          <div className="group flex justify-center">
+            <button data-tooltip-target="tooltip-default">
+              <InfoImg className="dark:stroke-white stroke-special-black" />
+            </button>
+            <div className="relative">
+              <span className="absolute w-40 -ml-20 transition-all duration-300 bottom-7 left-0 opacity-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:opacity-100">
+                This is a Catamaran Swap. You can exchange between BTC and STX.
+              </span>
+            </div>
+          </div>
         </div>
       </div>
       <div className="p-5 flex justify-between items-center rounded-lg bg-[rgba(7,7,10,0.03)] dark:bg-[#14151A] border-[1px] border-[rgba(7,7,10,0.1)] dark:border-[rgba(255,255,255,0.1)]">
